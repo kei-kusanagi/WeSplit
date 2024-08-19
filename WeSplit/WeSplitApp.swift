cd85b889c5a05bcde3ecbb0c947aa049f0b58dff
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-08-19 13:44:05 -0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Juan Carlos Robledo Morales on 19/08/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
